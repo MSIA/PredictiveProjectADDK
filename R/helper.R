1c51c790f9e7b8854791a1e269538a3c3607df52
kmeier1633 <kristinmeier2017@u.northwestern.edu>
2016-11-28 12:56:30 -0600

    added expected donation function

--- R/helper.R
+++ R/helper.R
@@ -6,7 +6,10 @@ library(pROC)
 # HELPER FUNCTIONS
 #######################
 
+#######################
 # AUC
+#######################
+
 auc <- function(model = logModel, 
                 testdata = donTEST,
                 testresponse = donTEST$donated){
@@ -19,7 +22,11 @@ auc <- function(model = logModel,
   return(auc)
 }
 
+
+#######################
 # CCR
+####################### 
+
 ccr <- function(model = logModel, 
                 testdata = donTEST,
                 testresponse = donTEST$donated,
@@ -30,4 +37,31 @@ ccr <- function(model = logModel,
   CCR = sum(diag(tab))/sum(tab)
   
   return(CCR)
+}
+
+#######################
+# Expected Donations (top 1000)
+#######################
+
+expected.don <- function(final.log.model = logModel,
+                         final.lm.model = lmModel,
+                         final.data = donTEST_orig){
+  #get probability that each person donates from logistic model
+  final.data$prob <- predict.glm(final.log.model, newdata=final.data, type="response")
+  
+  #get guess of each person's donation from multiple regression model
+  final.data$donGuess <- predict.lm(final.lm.model, newdata=final.data, na.action = na.pass)
+  final.data$donGuess <- ifelse(final.data$donGuess < 0, 0, final.data$donGuess)
+  
+  #get expected value of each person's donation
+  final.data$expDon <- final.data$prob * final.data$donGuess
+  sum.don <- summary(final.data$expDon)
+  
+  #Select 1000 donors from the test set who have the highest E(TARGDOL). These may be the donors
+  #that will be special marketing targets. Then find their total actual donations. This is the payoff
+  #and should be as high as possible.
+  top1000 <- final.data[order(final.data$expDon, decreasing=T),][1:1000,]
+  tot.don <- sum(top1000$TARGDOL) 
+  
+  return(tot.don)
 }
